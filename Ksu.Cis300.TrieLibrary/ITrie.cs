5ffca38913fd222c3d3674bdbbc88d4022b5a891
nazajac <nazajac@OBAMA>
2019-03-25 12:21:13 -0500

    Done

---
+++ Ksu.Cis300.TrieLibrary/ITrie.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// An interface for a trie.
+    /// </summary>
+    public interface ITrie
+    {
+        /// <summary>
+        /// Determines whether this trie contains the given string.
+        /// </summary>
+        /// <param name="s">The string to look for.</param>
+        /// <returns>Whether this trie contains s.</returns>
+        bool Contains(string s);
+
+        /// <summary>
+        /// Adds the given string to this trie.
+        /// </summary>
+        /// <param name="s">The string to add.</param>
+        /// <returns>The resulting trie.</returns>
+        ITrie Add(string s);
+    }
+
+    public class TrieWithNoChildren : ITrie
+    {
+        private bool _hasEmpty = false;
+        public ITrie Add(string s)
+        {
+
+            if (s == "")
+            {
+                _hasEmpty = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                TrieWithOneChild t = new TrieWithOneChild(s, _hasEmpty);
+                return t;
+            }
+            return this;
+        }
+
+        public bool Contains(string s)
+        {
+           return false;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    class TrieWithOneChild : ITrie
+    {
+        
+            private bool _hasEmptyString;
+            private ITrie _child;
+            private char _childLabel;
+
+            public TrieWithOneChild(string s, bool hasEmpty)
+            {
+                if (s == "" || s[0] < 'a' || s[0] > 'z')
+                {
+                    throw new ArgumentException();
+                }
+            _hasEmptyString = hasEmpty;
+            _childLabel = s[0];
+            _child = new TrieWithNoChildren().Add(s.Substring(1));
+            }
+
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else if (s[0].Equals(_childLabel))
+            {
+                _child = _child.Add(s.Substring(1));
+            }
+            else
+            {
+                return new TrieWithManyChildren(s, _hasEmptyString, _childLabel, _child);
+            }
+            return this;
+        }
+
+        public bool Contains(string s)
+            {
+                if (s == "")
+                {
+                    return false;
+                }
+                else if (s[0].Equals(_childLabel))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+    }
+
